b1304080ee978709102d3f092e0e2d25725dd49d
Himanshu <himanshu092017@gmail.com>
2021-11-17 17:11:11 +0530

    Loading Screen

--- src/Pages/TransactionData.jsx
+++ src/Pages/TransactionData.jsx
@@ -1,37 +1,54 @@
-import React from 'react'
-import { Card, CardBody } from 'reactstrap'
-import BuySellMonthlyChart from '../components/BuySellMonthly'
-import BuySellWeeklyChart from '../components/BuySellWeekly'
-import DepositChart from '../components/DepositWeekly'
-import DepositChartMonthly from '../components/DepositWithdrawMonthly'
-
+import React from 'react';
+import { Card, CardBody } from 'reactstrap';
+import BuySellMonthlyChart from '../components/BuySellMonthly';
+import BuySellWeeklyChart from '../components/BuySellWeekly';
+import DepositChart from '../components/DepositWeekly';
+import DepositChartMonthly from '../components/DepositWithdrawMonthly';
 
 function TransactionData() {
   return (
     <div>
       <div className='row'>
-      <Card style={{marginBottom:"10px",marginLeft:"10px",marginRight:"10px",marginTop:'10px'}}> 
-        <CardBody>
-          <BuySellWeeklyChart />
-        </CardBody>
-      </Card>
+        <Card
+          style={{
+            marginBottom: '10px',
+            marginLeft: '10px',
+            marginRight: '10px',
+            marginTop: '10px',
+          }}>
+          <CardBody>
+            <BuySellWeeklyChart />
+          </CardBody>
+        </Card>
       </div>
       <div className='row'>
-      <Card style={{marginBottom:"10px",marginLeft:"10px",marginRight:"10px",marginTop:'10px'}}> 
-        <CardBody>
-          <DepositChart />
-        </CardBody>
-      </Card>
+        <Card
+          style={{
+            marginBottom: '10px',
+            marginLeft: '10px',
+            marginRight: '10px',
+            marginTop: '10px',
+          }}>
+          <CardBody>
+            <DepositChart />
+          </CardBody>
+        </Card>
       </div>
-       <div className='row'>
-      <Card style={{marginBottom:"10px",marginLeft:"10px",marginRight:"10px",marginTop:'10px'}}> 
-        <CardBody>
+      <div className='row'>
+        <Card
+          style={{
+            marginBottom: '10px',
+            marginLeft: '10px',
+            marginRight: '10px',
+            marginTop: '10px',
+          }}>
+          <CardBody>
             <DepositChartMonthly />
           </CardBody>
-      </Card>
+        </Card>
       </div>
     </div>
-  )
+  );
 }
 
-export default TransactionData
+export default TransactionData;
